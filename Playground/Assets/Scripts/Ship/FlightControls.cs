0546dec58a0ccd694e9bd5ea5ff069ed368c2abc
Kyle Funka <kfunka@umd.edu>
2022-04-09 10:28:50 -0400

    ship and player controls separated by interacting with the game object with the flight controller script attached

--- Playground/Assets/Scripts/Ship/FlightControls.cs
+++ Playground/Assets/Scripts/Ship/FlightControls.cs
@@ -6,11 +6,27 @@ using UnityEngine.InputSystem;
 public class FlightControls : InteractableObject
 {
    private InputAction movement;
+   public GameObject rocketCam;
+   private PlayerInput playerInput;
+   private bool controllingShip;
+
    //[SerializeField]
    //private float speed = 10f;
    //[SerializeField]
    //private float mouseSensitivity = 1f;
+public void Start(){
+   controllingShip = false;
+}
    public override void Interact(PlayerInteraction presser) {
+      playerInput = presser.GetComponent<PlayerInput>();
+      presser.transform.rotation = this.transform.rotation;
       presser.transform.position = this.transform.position;
+      rocketCam.SetActive(true);
+      playerInput.SwitchCurrentActionMap("Ship");
+   }
+
+   public void LeaveCockpit(InputAction.CallbackContext context){
+      rocketCam.SetActive(false);
+      playerInput.SwitchCurrentActionMap("Player");
    }
 }
